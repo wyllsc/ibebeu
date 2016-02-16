0484d2d3f713b3a6466442a1c165e75fcb35dad5
Wylliam SantClair <wyllsc@gmail.com>
2016-02-16 19:34:24 -0200

    base material

---
+++ ibebeu-material/www/js/app.js
@@ -0,0 +1,77 @@
+var app = angular.module('starter', ['ionic', 'ionic-material']);
+
+app.run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
+})
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+
+    .state('app', {
+        url: '/app',
+        abstract: true,
+        templateUrl: 'templates/menu.html',
+        controller: 'AppCtrl'
+    })
+
+    .state('app.lists', {
+        url: '/lists',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/lists.html',
+                controller: 'ListsCtrl'
+            }
+        }
+    })
+
+    .state('app.ink', {
+        url: '/ink',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/ink.html',
+                controller: 'InkCtrl'
+            }
+        }
+    })
+
+    .state('app.motion', {
+        url: '/motion',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/motion.html',
+                controller: 'MotionCtrl'
+            }
+        }
+    })
+
+    .state('app.promocoes', {
+        url: '/promocoes',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/promocoes.html',
+                controller: 'PromocoesCtrl'
+            }
+        }
+    })
+
+    .state('app.extensions', {
+        url: '/extensions',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/extensions.html',
+                controller: 'ExtensionsCtrl'
+            }
+        }
+    })
+    ;
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/app/promocoes');
+});
